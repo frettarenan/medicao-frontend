f100a5ae81f2f333988a0427d17b5b47d14d8a2d
frettarenan <frettarenan@gmail.com>
2019-04-10 15:06:14 -0300

    teste de redirect de http para https

--- server.js
+++ server.js
@@ -1,13 +1,34 @@
-const express = require('express');
-const app = express();
 
-app.use(express.static(__dirname + '/dist'));
 
-app.get('*', function(req, res) {
-  if(req.headers['x-forwarded-proto'] != 'https')
-    res.redirect('https://' + req.get('host') + req.url);
-  else
-    res.sendFile(__dirname + '/dist/index.html');
+// Require and create the Express framework
+var express = require('express');
+var app = express();
+
+// Determine port to listen on
+app.listen(process.env.PORT || 4200);
+
+// Enable reverse proxy support in Express. This causes the
+// the "X-Forwarded-Proto" header field to be trusted so its
+// value can be used to determine the protocol. See 
+// http://expressjs.com/api#app-settings for more details.
+app.enable('trust proxy');
+
+// Add a handler to inspect the req.secure flag (see 
+// http://expressjs.com/api#req.secure). This allows us 
+// to know whether the request was via http or https.
+app.use(function (req, res, next) {
+  if (req.secure) {
+    // request was via https, so do no special handling
+    next();
+  } else {
+    // request was via http, so redirect to https
+    res.redirect('https://' + req.headers.host + req.url);
+  }
 });
 
-app.listen(process.env.PORT || 4200);
+// Allow static files in the /dist directory to be served
+app.use(express.static(__dirname + '/dist'));
+
+app.get('/*', function(req, res) {
+  res.sendFile(__dirname + '/dist/index.html');
+});
