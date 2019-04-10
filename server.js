ce3737dd1cdb7cf99c8ad2e912c60b0b2285e0ba
frettarenan <frettarenan@gmail.com>
2019-04-10 14:39:17 -0300

    teste de redirect de http para https

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const app = express();
 
 app.use(express.static(__dirname + '/dist'));
 
-app.get('/', function (req, res) {
+http.get('/', function (req, res) {
   res.redirect('https://' + req.get('host') + req.url);
-})
-
-app.get('/*', function(req, res) {
-  res.sendFile(__dirname + '/dist/index.html');
 });
+
+/*app.get('/*', function(req, res) {
+  res.sendFile(__dirname + '/dist/index.html');
+});*/
 
 app.listen(process.env.PORT || 4200);
